600b24de8ef327e7ff970b70965723ba36fdbfd5
matthewrapp <mattrapp25@gmail.com>
2021-02-17 11:34:51 -0700

    Food class done

--- solo-checkpoints/week7/snake/game/food.py
+++ solo-checkpoints/week7/snake/game/food.py
@@ -9,7 +9,41 @@ from game.point import Point
 class Food(Actor):
 
     def __init__(self):
-        _points = 0
+        '''
+        Food class constructor. Inherits from Actor class.
+        Sets the text and moves the food to different, random positions within
+        the game board in the terminal.
+
+        Args:
+            self(Actor): an instance of Actor
+        '''
+        super().__init__()
+        self._points = 0
+        self.set_text("@")
+        self.reset()
+
+    def get_points(self):
+        '''
+        Gets the points for when the snake gets the food.
+
+        Args:
+            self(Food): an instance of Food
+
+        Returns:
+            integer: The points each food is worth
+        '''
+        return self._points
 
     def reset(self):
-        pass
+        '''
+        Resets the food, then moves to a random position within the game board
+        in the terminal
+
+        Args:
+            self(Food): an instance of Food
+        '''
+        self._points = random.randint(1, 5)
+        x = random.randint(1, constants.MAX_X - 2)
+        y = random.randint(1, constants.MAX_Y - 2)
+        position = Point(x, y)
+        self.set_position(position)
