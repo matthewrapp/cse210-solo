125526400d41e0bdfc2aae54ff05dde0427e2991
matthewrapp <mattrapp25@gmail.com>
2021-02-17 10:32:58 -0700

    Pushing Snake

---
+++ solo-checkpoints/week7/snake/game/food.py
@@ -0,0 +1,15 @@
+import random
+from game import constants
+from game.actor import Actor
+from game.point import Point
+
+# TODO: Define the Food class here
+
+
+class Food(Actor):
+
+    def __init__(self):
+        _points = 0
+
+    def reset(self):
+        pass
